2521f0fcaeded04a689c1d9d97926b0a08065318
Steve Schrab <steve.schrab@gsdesign.com>
2015-09-18 11:27:00 -0500

    Add kerning class

--- sup-reg.js
+++ sup-reg.js
@@ -1,17 +1,23 @@
 /*
- * sup-reg.js v1.0.1
+ * sup-reg.js v1.1.0
  * https://github.com/gsmke/sup-reg.js
  */
 
 ;(function ($) {
-    'use strict';
+  'use strict';
 
-    $(document).ready(function () {
-        $('body :not(script,sup)').contents().filter(function () {
-            return this.nodeType === 3;
-        }).replaceWith(function () {
-            return this.nodeValue.replace(/[®]/g, '<sup>$&</sup>');
-        });
-    });
+  $('body :not(script,sup)').contents().filter(function () {
+    return this.nodeType === 3;
+  }).replaceWith(function () {
+    return this.nodeValue.replace(/[®]/g, '<sup>$&</sup>');
+  });
+  $('sup').each(function(index, el) {
+    var thisParent = $(el).parent().html();
+    var thisSup = thisParent.substr(0, thisParent.indexOf('<sup>')).slice(-1);
+    $(el).addClass('sup-reg');
+    if (thisSup === '.') {
+      $(el).addClass('sup-reg-kern');
+    }
+  });
 
 })(jQuery);
